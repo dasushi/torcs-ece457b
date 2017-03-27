556d4586076a82e2dc624ebb5627d34056b0565b
Stephen A <stephen.arsenault17@gmail.com>
2017-03-27 18:52:41 -0400

    New sub-project for commandline build

---
+++ bpnn_src/src/LibNN/neuron.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+#include "neuron.h"
+
+BPNeuralLink::BPNeuralLink(BPNeuron *in_neuron, 
+						   BPNeuron *out_neuron, 
+						   float in, float w_in, 
+						   float add): deltaw_prev(0.0f){
+	
+	input_neuron = in_neuron;
+	output_neuron = out_neuron;
+	in_val = in;
+	w = w_in;
+	in_add = add;
+}
+BPNeuralLink::~BPNeuralLink(){
+	//destructor
+}
+
+
+BPNeuron::BPNeuron: out_val(0), delta(0), function(SIGMOID){
+	//constructor
+}
+
+BPNeuron::~BPNeuron(){
+	//destructor
+	for(int i = 0; i < get_input_link_count() i++){
+		delete inputs[i];
+	}
+}
+
+void BPNeuron::add_input(BPNeuron *output_neuron) {
+
+	BPNeuralLink *link_ptr = new BPNeuralLink(this, output_neuron);
+	
+	inputs.push_back(link_ptr);
+	if(output_pointer){
+		output_pointer->outputs.push_back(link_ptr);
+	}
+}
+
---
+++ bpnn_src/src/stdafx.h
@@ -0,0 +1,38 @@
+#pragma once
+#pragma warning(disable : 4996)
+
+#include <iostream>
+#include <tchar.h>
+#include <math.h>
+#include <windows.h>
+#include <stdio.h>
+#include <time.h>
+#include <wchar.h>
+#include <conio.h>
+
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+//2D type array
+// size, filename, class type
+// N = record.size()
+// getting a point: record.entries[i].vec[j]
+// [entry, ... , vec, ..] 
+// y axis of each vector at each entry
+// vector<int> x0;  vector<int> x1; ... vector<int> xClassCount
+//
+typedef struct _entry {
+	float *vec;
+	float val;
+	int size;
+	wchar_t filename[_MAX_PATH];
+} ENTRY, *PENTRY;
+
+typedef struct _record {
+	vector<PENTRY> entries;
+	vector<vector<int>> indices;
+	vector<float> clasnum;
+} RECORD, *PRECORD;
+
